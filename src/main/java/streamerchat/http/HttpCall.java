79c423ba79856e66db45c53e7eea2518e2c3f2b9
Martin Peeters <406336@student.fontys.nl>
2020-01-13 13:26:23 +0100

    Removed some unneccessairy imports and added Sonar export to POM

--- src/main/java/streamerchat/http/HttpCall.java
+++ src/main/java/streamerchat/http/HttpCall.java
@@ -4,14 +4,12 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Collection;
 
 public class HttpCall {
