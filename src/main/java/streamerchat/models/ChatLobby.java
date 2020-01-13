5925445a29f7a1310966699935c8f2d8d6748a2e
Martin Peeters <406336@student.fontys.nl>
2020-01-13 13:56:49 +0100

    Small fix in ChatLobby model for better understanding in isUserAlreadyInLobby() method

--- src/main/java/streamerchat/models/ChatLobby.java
+++ src/main/java/streamerchat/models/ChatLobby.java
@@ -22,10 +22,13 @@ public class ChatLobby {
     public Collection<Session> getSessions() { return sessions; }
     public Collection<String> getChatLog() { return chatLog; }
 
-    public void addUser(Session session) throws Exception {
-        if (isUserAlreadyInLobby(session)) {
+    public void addUser(Session session) {
+        if (!this.isUserAlreadyInLobby(session)) {
             this.sessions.add(session);
             System.out.println("[LOG] User #" + session.getSessionId() + " joined ChatLobby " + this.displayname);
+        }
+        else {
+            throw new IllegalArgumentException("You've already joined this lobby!");
         }
     }
 
@@ -35,14 +38,14 @@ public class ChatLobby {
     /*------------------------------------------------------------------------*/
 
     // Field and parameter checking
-    private boolean isUserAlreadyInLobby(Session session) throws IllegalArgumentException {
+    private boolean isUserAlreadyInLobby(Session session) {
         if (!this.sessions.isEmpty()) {
             for (Session loopSession : this.sessions) {
                 if (loopSession.getSessionId().equals(session.getSessionId())) {
-                    throw new IllegalArgumentException("You've already joined this lobby!");
+                    return true;
                 }
             }
         }
-        return true;
+        return false;
     }
 }
